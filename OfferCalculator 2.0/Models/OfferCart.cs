1ae31fefb2ce738f990cbfb23da252cdeb3bae92
johan haak <johanhaaken@gmail.com>
2016-06-03 11:15:12 +0200

    Add Create date

--- OfferCalculator 2.0/Models/OfferCart.cs
+++ OfferCalculator 2.0/Models/OfferCart.cs
@@ -7,12 +7,18 @@ namespace OfferCalculator_2._0.Models
 {
     public class OfferCart
     {
+        private DateTime _date = DateTime.Now;
         public int ID { get; set; }
 
         public int OfferInformationID { get; set; }
 
         public virtual OfferInformationModels OfferInformation { get; set; }
 
+        public DateTime DateCreated
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
 
         public int Quantity { get; set; }
 
